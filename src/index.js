0744e187bcc01ddb0182548ce1e0bdd5f517a495
Amansingh2202 <789singhaman@gmail.com>
2025-06-15 19:26:23 +0530

    setting up sequalize

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const express =require('express');
+const bodyParser=require("body-parser")
+
+
+
+
 require('dotenv').config();
 
-const {PORT}=require('./config/serverConfig');
+const {PORT}=require('./config/serverConfig.js');
 
 const  setupAndStartServer =async () => {
     // create the express object 
     const app = express();
-
+     
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
 
     app.listen(PORT,()=>{
         console.log(`Server start at PORT = ${PORT}`)
     })
 
 }
+setupAndStartServer()
